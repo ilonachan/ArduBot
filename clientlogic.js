2d1a0ae168cbdad20692296f02d6cd05e11648e2
thepikafan <anton.schloemer@gmail.com>
2017-07-04 11:45:58 +0200

    Add files via upload

---
+++ clientlogic.js
@@ -0,0 +1,81 @@
+function anfordern(mode) {
+	var req = new XMLHttpRequest();
+	req.open("get", "request.php?comm="+mode, true);
+	req.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+	// console.log("Sending request: 'request.php?comm="+mode+"'");
+	// if(mode == "refresh") {
+		// req.onreadystatechange = function(e){
+			// if(e.target.readyState == 4 && e.target.status == 200) {
+				// console.log(e.target.responseText);
+				// refreshView(e.target.responseText);
+			// }
+		// }
+	// } else {
+		req.onreadystatechange = auswerten;
+	// }
+	req.send();
+}
+
+function auswerten(e) {
+	if(e.target.readyState == 4 && e.target.status == 200)
+		console.log(e.target.responseText);
+}
+
+document.addEventListener("keydown", checkKeyDown);
+document.addEventListener("keyup", checkKeyUp);
+
+// setInterval(everySec,1000);
+
+// function refreshView(info){
+	// document.getElementById("idStatus").innerHtml = info;
+// }
+
+// function everySec() {
+	// // console.log("refresing");
+	// anfordern("refresh");
+// }
+
+var dirs = {
+	Up:0, Down:0, Left:0, Right:0
+};
+
+function setDir(dir, bool) {
+	if(dirs[dir] == bool)
+		return;
+	dirs[dir] = bool;
+	anfordern("move-"+dir+"-"+bool);
+}
+
+function checkKeyDown(e) {
+
+	e = e || window.event;
+	
+	var str;
+	switch(e.keyCode) {
+		case 38: str="Up"; break;
+		case 40: str="Down"; break;
+		case 37: str="Left"; break;
+		case 39: str="Right"; break;
+		default:
+			return;
+	}
+	document.getElementById("buttonSteer"+str).classList.add("activate");
+	setDir(str,1);
+}
+
+function checkKeyUp(e) {
+
+	e = e || window.event;
+	
+	var str;
+	switch(e.keyCode) {
+		case 38: str="Up"; break;
+		case 40: str="Down"; break;
+		case 37: str="Left"; break;
+		case 39: str="Right"; break;
+		default:
+			return;
+	}
+	document.getElementById("buttonSteer"+str).classList.remove("activate");
+	setDir(str,0);
+}
