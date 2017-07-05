23240e2db9654c4e8a66e374ae98f048ff7447ed
thepikafan <anton.schloemer@gmail.com>
2017-07-05 08:52:37 +0200

    Update wheel.js

--- wheel/wheel.js
+++ wheel/wheel.js
@@ -69,7 +69,114 @@ function Wheel(imageSource, axisX, axisY, options) {
 	this.rotationPoint = new Vector(); // Point of rotation in the document.
 	this.originToBegin = null;
 	this.canvas = canvas;
+	
+	
+	this.maxSteerAngle = 90;
 
+	this.onSteer = function (callback) {
+		this.onSteerFunc = callback;
+	};
+
+	this.steer = function (angle) {
+		this.canvas.style.transform = 'rotate(' + angle + 'deg)';
+		this.currentAngle = angle;
+		if (this.onSteerFunc)
+			this.onSteerFunc({
+				angle: angle < this.maxSteerAngle ? angle : -360 + angle
+			});
+	};
+
+	this.update = function () {
+		this.canvas.style.marginLeft = -this.canvas.clientWidth / 2 + 'px';
+		this.canvas.style.marginTop = -this.canvas.clientHeight / 2 + 'px';
+		const rect = this.canvas.getBoundingClientRect();
+		this.rotationPoint.x = rect.left + this.canvas.clientWidth / 2;
+		this.rotationPoint.y = rect.top + this.canvas.clientHeight / 2;
+	};
+
+	this.dragstart = function (e) {
+		this.dragPoint.x = e.pageX;
+		this.dragPoint.y = e.pageY;
+		this.originToBegin = Vector.sub(this.dragPoint, this.rotationPoint);
+
+		// Only accept clicking/tapping inside the radius of the wheel
+		if (this.radius > 0 && this.originToBegin.mag() > this.radius)
+			return;
+		e.preventDefault();
+		this.dragging = true;
+	};
+
+	this.dragend = function (e) {
+		if (!this.dragging)
+			return;
+		e.preventDefault();
+
+		this.desiredAngle = 0;
+		this.angle = this.currentAngle;
+
+		const self = this;
+		const interval = setInterval(function () {
+			const angle = lerp(self.angle, self.angle < 180 ? 0 : 360, 0.2);
+			if (self.dragging) {
+				clearInterval(interval);
+				return;
+			}
+
+			if (angle < 1 || angle > 360 - 1) {
+				self.angle = 0;
+				this.currentAngle = 0;
+				self.steer(self.angle);
+				clearInterval(interval);
+				return;
+			}
+
+			self.angle = angle;
+			self.steer(angle);
+		}, 1000/60);
+
+		this.dragging = false;
+
+		if (DEBUG) {
+			DEBUG_CTX.clearRect(0, 0, DEBUG_CANVAS.width, DEBUG_CANVAS.height);
+		}
+	};
+
+	this.dragmove = function (e) {
+		if (!this.dragging)
+			return;
+		e.preventDefault();
+
+		const current = new Vector(e.pageX, e.pageY);
+		const originToCurrent = Vector.sub(current, this.rotationPoint);
+		const angle = degrees(radiansPositive(Vector.angle(this.originToBegin, originToCurrent)));
+
+		if (angle > this.maxSteerAngle && angle < 360 - this.maxSteerAngle)
+			return;
+		this.steer(angle);
+
+		if (DEBUG) {
+			DEBUG_CTX.clearRect(0, 0, DEBUG_CANVAS.width, DEBUG_CANVAS.height);
+			DEBUG_CTX.beginPath();
+			DEBUG_CTX.moveTo(this.rotationPoint.x, this.rotationPoint.y);
+			DEBUG_CTX.lineTo(this.dragPoint.x, this.dragPoint.y);
+			DEBUG_CTX.moveTo(this.rotationPoint.x, this.rotationPoint.y);
+			DEBUG_CTX.lineTo(current.x, current.y);
+			DEBUG_CTX.lineTo(this.dragPoint.x, this.dragPoint.y);
+			DEBUG_CTX.stroke();
+		}
+	};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	// Once the document is loaded the state of the wheel can be updated,
 	// because it needs the `clientWidth` attribute of the canvas element.
 	document.addEventListener("DOMContentLoaded", function () { self.update(); });
@@ -87,97 +194,4 @@ function Wheel(imageSource, axisX, axisY, options) {
 	window.addEventListener("touchend", function (e) { self.dragend(e); }, false);
 }
 
-Wheel.prototype.maxSteerAngle = 90;
 
-Wheel.prototype.onSteer = function (callback) {
-	this.onSteerFunc = callback;
-};
-
-Wheel.prototype.steer = function (angle) {
-	this.canvas.style.transform = 'rotate(' + angle + 'deg)';
-	this.currentAngle = angle;
-	if (this.onSteerFunc)
-		this.onSteerFunc({
-			angle: angle < this.maxSteerAngle ? angle : -360 + angle
-		});
-};
-
-Wheel.prototype.update = function () {
-	this.canvas.style.marginLeft = -this.canvas.clientWidth / 2 + 'px';
-	this.canvas.style.marginTop = -this.canvas.clientHeight / 2 + 'px';
-	const rect = this.canvas.getBoundingClientRect();
-	this.rotationPoint.x = rect.left + this.canvas.clientWidth / 2;
-	this.rotationPoint.y = rect.top + this.canvas.clientHeight / 2;
-};
-
-Wheel.prototype.dragstart = function (e) {
-	this.dragPoint.x = e.pageX;
-	this.dragPoint.y = e.pageY;
-	this.originToBegin = Vector.sub(this.dragPoint, this.rotationPoint);
-
-	// Only accept clicking/tapping inside the radius of the wheel
-	if (this.radius > 0 && this.originToBegin.mag() > this.radius)
-		return;
-	e.preventDefault();
-	this.dragging = true;
-};
-
-Wheel.prototype.dragend = function (e) {
-	if (!this.dragging)
-		return;
-	e.preventDefault();
-
-	this.desiredAngle = 0;
-	this.angle = this.currentAngle;
-
-	const self = this;
-	const interval = setInterval(function () {
-		const angle = lerp(self.angle, self.angle < 180 ? 0 : 360, 0.2);
-		if (self.dragging) {
-			clearInterval(interval);
-			return;
-		}
-
-		if (angle < 1 || angle > 360 - 1) {
-			self.angle = 0;
-			this.currentAngle = 0;
-			self.steer(self.angle);
-			clearInterval(interval);
-			return;
-		}
-
-		self.angle = angle;
-		self.steer(angle);
-	}, 1000/60);
-
-	this.dragging = false;
-
-	if (DEBUG) {
-		DEBUG_CTX.clearRect(0, 0, DEBUG_CANVAS.width, DEBUG_CANVAS.height);
-	}
-};
-
-Wheel.prototype.dragmove = function (e) {
-	if (!this.dragging)
-		return;
-	e.preventDefault();
-
-	const current = new Vector(e.pageX, e.pageY);
-	const originToCurrent = Vector.sub(current, this.rotationPoint);
-	const angle = degrees(radiansPositive(Vector.angle(this.originToBegin, originToCurrent)));
-
-	if (angle > this.maxSteerAngle && angle < 360 - this.maxSteerAngle)
-		return;
-	this.steer(angle);
-
-	if (DEBUG) {
-		DEBUG_CTX.clearRect(0, 0, DEBUG_CANVAS.width, DEBUG_CANVAS.height);
-		DEBUG_CTX.beginPath();
-		DEBUG_CTX.moveTo(this.rotationPoint.x, this.rotationPoint.y);
-		DEBUG_CTX.lineTo(this.dragPoint.x, this.dragPoint.y);
-		DEBUG_CTX.moveTo(this.rotationPoint.x, this.rotationPoint.y);
-		DEBUG_CTX.lineTo(current.x, current.y);
-		DEBUG_CTX.lineTo(this.dragPoint.x, this.dragPoint.y);
-		DEBUG_CTX.stroke();
-	}
-};
